13932cd4097bf48f9e0051adfc3c581e9e09c675
Lorenzo Bassetti <lbassetti@gmail.com>
2024-11-06 22:27:40 +0100

    contributions

---
+++ models/base/base_capital_call_schedule.sql
@@ -0,0 +1,12 @@
+-- models/base/base_capital_call_schedule.sql
+
+WITH source_data AS (
+    SELECT
+        CAST(asset_class AS VARCHAR) AS asset_class,
+        CAST(geo_area AS VARCHAR) AS geo_area,
+        CAST(call_year AS INT) AS year,
+        CAST(call_value AS FLOAT) AS call_value
+    FROM {{ source('postgres', 'capital_call_schedule') }}
+)
+
+SELECT * FROM source_data
