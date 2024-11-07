d87260e3c6e0207d936c70bc53a866a8c191183d
Lorenzo Bassetti <lbassetti@gmail.com>
2024-11-07 18:47:53 +0100

    geo_region

--- models/base/base_pe_capital_call_schedule.sql
+++ models/base/base_pe_capital_call_schedule.sql
@@ -3,7 +3,7 @@
 WITH source_data AS (
     SELECT
         CAST(asset_class AS VARCHAR) AS asset_class,
-        CAST(geo_area AS VARCHAR) AS geo_area,
+        CAST(geo_region AS VARCHAR) AS geo_region,
         CAST(call_year AS INT) AS call_year,
         CAST(call_value AS FLOAT) AS call_value
     FROM {{ source('postgres', 'pe_capital_call_schedule') }}
--- models/base/base_pe_returns.sql
+++ models/base/base_pe_returns.sql
@@ -4,7 +4,7 @@ WITH source_data AS (
     SELECT
         CAST(vintage_year AS INT) AS vintage_year,
         CAST(asset_class AS VARCHAR) AS asset_class,
-        CAST(geo_area AS VARCHAR) AS geo_area,
+        CAST(geo_region AS VARCHAR) AS geo_region,
         CAST(first_transaction_period AS VARCHAR) AS first_transaction_period,
         CAST(as_of_date AS DATE) AS as_of_date,
         CAST(fund_count AS FLOAT) AS fund_count,
